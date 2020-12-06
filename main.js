334dd992a47b8f503c8723e0ab1b86525c1c73e7
dParikesit <dimasparikesitt@gmail.com>
2020-12-06 13:37:51 +0700

    Fix Webhook

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ app.post('/webhook', middleware(config), (req, res) => {
 function handleEvent(event) {
     if (event.type !== 'message' || event.message.type !== 'text') {
       // ignore non-text-message event
-      return Promise.resolve(null);
+      return {};
     }
   
     // create a echoing text message
