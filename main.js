ba0c082ec0aa6e5081e466691a8ceb438efd4955
dParikesit <dimasparikesitt@gmail.com>
2020-12-06 20:23:18 +0700

    Please be fixed

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const line = require('@line/bot-sdk')
 const crypto = require('crypto')
+const bodyParser = require('body-parser')
 const config = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
     channelSecret: process.env.CHANNEL_SECRET
@@ -13,13 +14,13 @@ const JSONParseError = line.JSONParseError
 const client = new line.Client(config)
 
 const app = express();
-app.use(middleware(config))
 
 app.post('/webhook', middleware(config), (req, res) => {
     const body = JSON.stringify(req.body)
     const signature = crypto
-        .createHmac('SHA256', channelSecret)
-        .update(body).digest('base64');
+      .createHmac('SHA256', channelSecret)
+      .update(body).digest('base64');
+    line.validateSignature(body,config.channelSecret,signature)
     if(req.body.events.length()==0){
         res.send(200)
     }
