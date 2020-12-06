5d47b926781b984debd54e13ea2075116487d0ad
dParikesit <dimasparikesitt@gmail.com>
2020-12-06 13:08:22 +0700

    Fix Webhook

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
+'use strict'
+
 const express = require('express')
 const line = requrie('@line/bot-sdk')
-const middleware = require('@line/bot-sdk').middleware
-const JSONParseError = require('@line/bot-sdk').JSONParseError
-
+const crypto = require('crypto')
 const config = {
     channelAccessToken: process.env.accessToken,
     channelSecret: process.env.secret
 }
 
+const middleware = line.middleware
+const JSONParseError = line.JSONParseError
+const client = new line.Client(config)
+
 const app = express();
 app.use(middleware(config))
 
 app.post('/webhook', middleware(config), (req, res) => {
-    req.body.events
-    req.body.destination
-    res.status(200)
+    Promise
+        .all(req.body.events.map(handleEvenet))
+        .then((result) => res.json(result))
+        .catch((err)=>{
+            console.error(err);
+            res.status(500).end();
+        });
 })
 
-app.listen(8080)
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      // ignore non-text-message event
+      return Promise.resolve(null);
+    }
+  
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+  
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+  }
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
