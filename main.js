0570830fbf084b19cfa012fe9fbea0acc4174e36
dParikesit <dimasparikesitt@gmail.com>
2020-12-06 19:51:23 +0700

    Another attempt

--- main.js
+++ main.js
@@ -15,30 +15,11 @@ const client = new line.Client(config)
 const app = express();
 app.use(middleware(config))
 
-app.post('/webhook', line.middleware(config), (req, res) => {
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result))
-      .catch((err) => {
-        console.error(err);
-        res.status(500).end();
-      });
-});
-  
-  // event handler
-function handleEvent(event) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-      // ignore non-text-message event
-      return Promise.resolve(null);
+app.post('/webhook', middleware(config), (req, res) => {
+    if(req.body.events.length()==0){
+        res.statusCode(200)
     }
-  
-    // create a echoing text message
-    const echo = { type: 'text', text: event.message.text };
-  
-    // use reply API
-    return client.replyMessage(event.replyToken, echo);
-}
-
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {
