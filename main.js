22f7fa9f36c229c8c299c246e620d3652288428c
dParikesit <dimasparikesitt@gmail.com>
2020-12-09 13:15:45 +0700

    Fix error

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 const express = require('express')
 const line = require('@line/bot-sdk')
-const client = new line.Client(config)
 const fs = require('fs')
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET
 }
+const client = new line.Client(config)
 
 const firebase = require('firebase/app')
 require('firebase/storage')
